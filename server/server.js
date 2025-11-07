bf9a35a5291b1268ab16ff7b82af92c1984e1e97
Armaan Siddiqui <armaansiddiqui.pms@gmail.com>
2025-11-07 20:07:20 +0530

    Allowed origin changed

--- server/server.js
+++ server/server.js
@@ -36,8 +36,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    // origin: "http://localhost:5173", // frontend during development
-    origin: "*",
+    origin: "http://localhost:5173", // frontend during development
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
