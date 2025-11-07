d83ca62317b4058fa9751211594e5be54c9f29d2
Armaan Siddiqui <armaansiddiqui.pms@gmail.com>
2025-11-07 19:54:23 +0530

    Filter in bulk products edit modified

--- server/server.js
+++ server/server.js
@@ -36,7 +36,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend during development
+    // origin: "http://localhost:5173", // frontend during development
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
