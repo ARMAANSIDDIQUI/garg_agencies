a397bcc23e41bd38191e33b6e57d1e46f63ffffb
Armaan Siddiqui <armaansiddiqui.pms@gmail.com>
2026-07-06 18:20:39 +0530

    update

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dummy_cloud_name",
-  api_key: process.env.CLOUDINARY_API_KEY || "dummy_api_key",
-  api_secret: process.env.CLOUDINARY_API_SECRET || "dummy_api_secret",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "",
+  api_key: process.env.CLOUDINARY_API_KEY || "",
+  api_secret: process.env.CLOUDINARY_API_SECRET || "",
   secure: true // Ensures the returned URL is HTTPS
 });
 //
